09b49b5b90080873da490902c32f6789c8a3c15c
Candace Eckels <candace.eckels@gmail.com>
2022-10-06 19:08:53 -0600

    Tested append(data) method in Node class

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,4 +1,5 @@
 require "./lib/node"
+require "./lib/linked_list"
 
 RSpec.describe Node do
   describe 'Exists' do
@@ -8,6 +9,16 @@ RSpec.describe Node do
       expect(node).to be_a Node
       expect(node.data).to eq "plop"
       expect(node.next_node).to be_nil
+    end
+  end
+
+  describe '#append(data)' do
+    it 'can assign a node to next_node value' do
+      node = Node.new("plop")
+
+      new_node = node.append("boo")
+
+      expect(node.next_node).to eq new_node
     end
   end
 end
